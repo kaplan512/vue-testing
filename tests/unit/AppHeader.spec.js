b731a9d069573581cef5ab5a2516c01607474000
Yaroslav Sayenko <y.sayenko@theappsolutions.com>
2019-10-25 16:32:52 +0300

    initial commit

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+import AppHeader from "../../src/components/AppHeader";
+
+describe("AppHeader", () => {
+    test("if logged in is false, do not show logout button", () => {
+        const wrapper = mount(AppHeader);
+        expect(wrapper.find("button").isVisible()).toBe(false);
+    });
+
+    test("if logged in, show logout button", () => {
+        const wrapper = mount(AppHeader);
+        wrapper.setData({ loggedIn: true });
+        expect(wrapper.find("button").isVisible()).toBe(true);
+    });
+});
